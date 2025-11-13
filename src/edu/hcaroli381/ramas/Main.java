0f249c9a62927f58f2e4167a3ae531f4c415cdab
hcaroli381 <hcaroli381@g.educaand.es>
2025-11-13 10:44:47 +0100

    Suma de numeros principales

--- src/edu/hcaroli381/ramas/Main.java
+++ src/edu/hcaroli381/ramas/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Lotería Primitiva: 5 números principales (1..49) + 1 complementario
 		HashSet<Integer> bolas = new HashSet<>();
+
 		Random r = new Random();
 		while (bolas.size() < 5) {
 			bolas.add(r.nextInt(49) + 1);
@@ -14,5 +15,7 @@ public class Main {
 		System.out.println("Números principales: " + bolas);
 		int complementario = r.nextInt(49) + 1;
 		System.out.println("Complementario: " + complementario);
+		int suma = bolas.stream().mapToInt(Integer::intValue).sum();
+		System.out.println("Suma de principales: " + suma);
 	}
 }
